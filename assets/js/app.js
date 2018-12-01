3b338a15b4e7598ec0294361b2531056f789f269
Gabriela Latín <gabriela@digitalbee.cl>
2018-11-30 22:22:47 -0300

    pirinola con js

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $("#iz").on("click", function() {
+    $("#banner").attr("src", "assets/img/banner_2.jpg");
+  });
+
+  $("#der").on("click", function() {
+    $("#banner").attr("src", "assets/img/banner_1.jpg");
+  });
+
+
+})
