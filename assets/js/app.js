9f8bc954e1684475f41d5c5f3e80ac2c5af049a6
Gabriela Latín <gabriela@digitalbee.cl>
2018-12-03 10:36:25 -0300

    con galeria de imágenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,5 +8,10 @@ $(document).ready(function() {
     $("#banner").attr("src", "assets/img/banner_1.jpg");
   });
 
+  $(".featured").attr("src", "assets/img/festicrin_1.jpg")
 
+  $(".thumb").on("click", function() {
+    source = $(this).attr("src");
+    $(".featured").attr("src", source);
+  })
 })
